367b2fa05b9c76ad37b7e776f4179fa4c2ca6852
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-04-24 12:08:38 +0200

    Extended example for the unit test configuration

--- Tests/CoreAPNsTests/TestConfig.example.swift
+++ Tests/CoreAPNsTests/TestConfig.example.swift
@@ -10,6 +10,8 @@ import Foundation
 /// TODO: Rename this file to TestConfig
 class TestConfigExample {
 
+    // MARK: - Required information to create an access token for the APNs Service
+
     /// the TEAM-ID Must be set and is used as issuer of the notification.
     static let teamID     = "YOUR_TEN_LETTER_TEAM_ID"
 
@@ -19,4 +21,15 @@ class TestConfigExample {
     // Your Key-File. Must be created and downloaded. See: Apple Developer Account / Certificates, Identifiers & Profiles / Keys
     // IMPORTANT: This file path must not be located in a protected directory like Downloads
     static let p8FilePath = "LOCAL_PATH/AuthKey_<KEY-ID>.p8"
+
+    // MARK: - required information to create a notification
+
+    // This identifier must be created by the app. See: https://stackoverflow.com/questions/25925481/how-to-get-a-unique-device-id-in-swift
+    static let deviceID    = "YOUR_DEVICE_ID"
+
+    // Application Bundle Identifier from your App. See: Project / Target / General / Bundle Identifier
+    static let appBundleID = "YOUR_APP_BUNDLE_ID"
+
+    // Payload of the notification
+    static let payload     = "{\"aps\":{\"alert\":\"Test\"},\"yourCustomKey\":\"1\"}"
 }
